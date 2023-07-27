6941275329184b53bd8653c30e2c80d7167c33c1
Jean kersen Villefranche <jvillefr@c2r3p8.42quebec.com>
2023-07-27 14:22:09 -0400

    1rst commit

---
+++ includes/my_lib/src/char_functions.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   char_functions.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alexnshimiyimana <alexnshimiyimana@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/11 16:10:00 by anshimiy          #+#    #+#             */
+/*   Updated: 2023/07/18 12:14:38 by alexnshimiy      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../mylib.h"
+
+int	ft_printchar(char c, int fd)
+{
+	return (write(fd, &c, 1));
+}
+
+// NULL = return 6 bcuz NULL=4 and the %s =
+int	ft_printstr(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	if (s == NULL)
+	{
+		ft_printstr("(null)", 1);
+		return (6);
+	}
+	while (s[i])
+	{
+		ft_putchar_fd(s[i], fd);
+		i++;
+	}
+	return (i);
+}
+
+void	ft_print_array(char **arr)
+{
+	int	i;
+
+	i = 0;
+	write(1, "[\n", 2);
+	while (arr[i])
+	{
+		write(1, "\t[", 2);
+		write(1, arr[i], ft_strlen(arr[i]));
+		write(1, "]\n", 2);
+		i++;
+	}
+	write(1, "]\n", 2);
+}
+
+void	ft_print_int_array(int **arr, int max_size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	write(1, "[\n", 2);
+	while (arr[i])
+	{
+		write(1, "\t[", 2);
+		j = 0;
+		while (arr[i][j] || j < max_size)
+		{
+			ft_putnbr_fd(arr[i][j], 1);
+			j++;
+		}
+		write(1, "]\n", 2);
+		i++;
+	}
+	write(1, "]\n", 2);
+}
---
+++ src/utils/map_position.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_position.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alexnshimiyimana <alexnshimiyimana@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/30 15:24:57 by anshimiy          #+#    #+#             */
+/*   Updated: 2023/07/22 22:48:45 by alexnshimiy      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/cub3d.h"
+
+/**
+ * @brief  Return the position of the map in the file_data
+ * @note   TODO: make sure this function is valid and don't need more...
+ * @param  file_data:
+ * @retval
+ */
+int	ft_find_map_position(char **file_data)
+{
+	int	i;
+
+	i = 0;
+	while (file_data[i])
+	{
+		if (file_data[i][0] && ft_str_made_of_set(file_data[i], "\t 1"))
+		{
+			return (i);
+		}
+		i++;
+	}
+	return (-1);
+}
+
+/**
+ * @brief  Checks if the block to be deleted in the view is valid
+ * @note   if itss a wall that's not a border, it's valid
+ * @todo   Instead of checking based on the minimap ray position,
+ *         check based on the a raycaster for it
+ * @param  *rc:
+ * @param  to_remove_x:
+ * @param  to_remove_y:
+ * @retval 1 if invalid, 0 if valid
+ */
+int	block_forbidden(t_raycaster_data *rc, int to_remove_x, int to_remove_y)
+{
+	if (!to_remove_x || !to_remove_y || to_remove_x > rc->map_width - 2
+		|| to_remove_y > rc->map_height - 2)
+	{
+		return (1);
+	}
+	else if (rc->map[to_remove_y + 1][to_remove_x] == 2 || rc->map[to_remove_y
+		- 1][to_remove_x] == 2)
+	{
+		return (1);
+	}
+	else if (rc->map[to_remove_y][to_remove_x + 1] == 2
+		|| rc->map[to_remove_y][to_remove_x - 1] == 2)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
